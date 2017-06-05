0870956e9e64a6eef7d8b2395152cfe44f770959
Cor Bouwhuis <corbouw@yahoo.com>
2017-06-05 13:05:27 +0200

    Thermometer barometer
    Waarden van BMP280 temperatuur- en luchtdruksensor doorgeven als LoRaWAN payload

--- LoRaWAN_TTN_Enschede.ino
+++ LoRaWAN_TTN_Enschede.ino
@@ -55,6 +55,11 @@
 * Removed a bug from the Waitloop file
 * Now using AppSkey in Encrypt payload function (Thanks to Willem4ever)
 * Switching to standby at start of send package function
+*
+* Firmware version 3.2
+* Merged I2C_ReadAllData.ino from 
+* https://github.com/sparkfun/SparkFun_BME280_Arduino_Library
+* for interfacing with the BMP280
 ****************************************************************************************/
 
 /*
@@ -62,6 +67,7 @@
 * INCLUDE FILES
 *****************************************************************************************
 */
+#include "Wire.h"
 #include <SPI.h>
 #include "AES-128_V10.h"
 #include "Encrypt_V31.h"
@@ -69,6 +75,9 @@
 #include "RFM95_V21.h"
 #include "LoRaMAC_V11.h"
 #include "Waitloop_V11.h"
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
 
 /*
 *****************************************************************************************
@@ -88,9 +97,24 @@ unsigned char NwkSkey[16] = { 0x06, 0x16, 0x67, 0x62, 0x10, 0x0B, 0xD7, 0xDD, 0x
 unsigned char AppSkey[16] = { 0x9C, 0x82, 0x5A, 0x25, 0x06, 0xAA, 0xBD, 0xDA, 0xC5, 0x20, 0xA4, 0x93, 0x49, 0x5B, 0x21, 0x28 };
 unsigned char DevAddr[4] = { 0x26, 0x01, 0x11, 0x35 };
 
+// Knippen en plakken van 
+//    https://console.thethingsnetwork.org/applications/cb_sensor/devices/sensor1
+
+// const char *devAddr = "26011135";
+// const char *nwkSKey = "06166762100BD7DD8AC9A39AF605DA57";
+// const char *appSKey = "9C825A2506AABDDAC520A493495B2128";
 
 int FC = 0;
 // SF to set in LoRaWAN)V31.h (default SF9 is set) - quick and dirty implemented (TBC)
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11 
+#define BMP_CS 10
+
+Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 void setup() 
 {
@@ -113,13 +137,23 @@ void setup()
   pinMode(CS,OUTPUT);
   
   digitalWrite(CS,HIGH);
-
+  
   WaitLoop_Init();
 
   //Wait until RFM module is started
   WaitLoop(20);   
   Serial.println("Setup: Completed");
   Serial.println("---");
+
+  if (!bmp.begin()) {  
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+
+
+  Serial.print("Program Started\n");
+    
+  Serial.println();
 }
 
 void loop() 
@@ -141,9 +175,19 @@ void loop()
   delay(1000);
 
     // Here the sensor information should be retrieved 
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+    
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure() / 100.0);
+    Serial.println(" hPa");
+  
+  Serial.println();
    
     //Construct data 
     sprintf(msg,"Hello world!");
+    sprintf(msg,"%d.%02d C %ld.%02ld hPa", (int)bmp.readTemperature(), (int)(bmp.readTemperature()*100)%100, (long)bmp.readPressure()/100, (long)bmp.readPressure()%100);
     memcpy(Data_Tx, msg, strlen(msg));
     Data_Length_Tx = strlen(msg);
 
